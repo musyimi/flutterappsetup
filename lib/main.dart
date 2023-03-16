f43dff0d80d117b13f938f4ac46dd2a00cb3366e
Musyimi <musyimi13@gmail.com>
2023-03-16 15:46:09 +0300

    added image icons for ios and android devices

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,7 @@ void main() {
           backgroundColor: Colors.blueGrey[900],
         ),
         body: Image(
-          image: NetworkImage(
-              'https://images.unsplash.com/photo-1588515603140-81bd9f7d1db0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cGhvbmUlMjByZXBhaXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'),
+          image: AssetImage('images/comprep.jpg'),
         ),
       ),
     ),
